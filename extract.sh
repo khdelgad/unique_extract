e4f709ef2fe56d3e10ae8fca61beb10ead96afbf
yggrdrasil <valentina.corradi@gmail.com>
2015-10-06 15:54:47 -0600

    Killed typos, :)

--- extract.sh
+++ extract.sh
@@ -1,4 +1,5 @@
-Karelia Delgado, Octuber 6th, 2015
+## Karelia Delgado, October 6th, 2015
+### Mycollaborator is awsome!!!
 
 cut -f $1 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
 
