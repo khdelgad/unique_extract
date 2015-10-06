9fae59445190e0e2cd6d58d3c0ef7ef705be88bf
yggrdrasil <valentina.corradi@gmail.com>
2015-10-06 15:57:59 -0600

    I've added 2 more comments

--- extract.sh
+++ extract.sh
@@ -1,6 +1,10 @@
 ## Karelia Delgado, October 6th, 2015
 ### My collaborator is awesome!!!
 
+### My collaborator is really really awesome!!!
+
+## Now the seriuos stuff
+
 cut -f $1 -d',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
 
 
